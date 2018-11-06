41f103bb1b0642b764ce791170fa21dbf43124d1
sudharsanqa <sudharsan.avula@fissionlabs.in>
2018-11-06 08:22:47 +0530

    updating project

---
+++ FirefoxBrowser.java
@@ -0,0 +1,39 @@
+
+package com.framework.core;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import com.framework.helper.ResourceHelper;
+
+
+
+public class FirefoxBrowser {
+	
+	public Capabilities getFirefoxCapabilities() {
+		DesiredCapabilities firefox = DesiredCapabilities.firefox();
+		FirefoxProfile profile = new FirefoxProfile();
+		profile.setAcceptUntrustedCertificates(true);
+		profile.setAssumeUntrustedCertificateIssuer(true);
+		firefox.setCapability(FirefoxDriver.PROFILE, profile);
+		firefox.setCapability("marionette", false);
+		//firefox.setCapability("marionette", true);
+		return firefox;
+	
+	}
+	
+	public WebDriver getFirefoxDriver(Capabilities cap) {
+		System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/DriverFiles/geckodriver.exe");
+		return new FirefoxDriver(cap);
+	}
+	
+	public WebDriver getFirefoxDriver(String hubUrl,Capabilities cap) throws MalformedURLException {
+		return new RemoteWebDriver(new URL(hubUrl),cap);
+	}
+
+}
